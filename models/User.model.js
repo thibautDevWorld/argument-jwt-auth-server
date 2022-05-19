b96f76c244fc2b2e67457400fbd03982d5a544f1
Thibaut Lartigue <lartiguethibaut@gmail.com>
2022-05-19 11:18:28 +0200

    Preparing server for the project 3

---
+++ models/User.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+});
+
+module.exports = model("User", userSchema);
